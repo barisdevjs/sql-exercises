8752c4d97214e5429b59f904b958a2194c33c36b
Barış SAVAŞ <barissavas17@gmail.com>
2023-04-08 15:32:28 +0300

    added second solution

--- Hackerrank/Medium/SqlProjectPlanning.sql
+++ Hackerrank/Medium/SqlProjectPlanning.sql
@@ -6,4 +6,22 @@ GROUP BY Start_Date
 ORDER BY DATEDIFF(MIN(End_Date), Start_Date), Start_Date; 
 
 
+with
+t1 as 
+(select start_date, row_number() over(order by start_date) rn1
+from projects
+where start_date not in (select end_date from projects)),
+
+t2 as 
+(select end_date, row_number() over(order by end_date) rn2
+from projects
+where end_date not in (select start_date from projects))
+
+select t1.start_date, t2.end_date
+from t1 join t2 
+on t1.rn1 = t2.rn2
+order by (t2.end_date-t1.start_date), start_date
+
+
+
 
